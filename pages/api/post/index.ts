f123b28774c985256a73e6ad5b3a9ed33e47943b
John Denlinger <john_d@dc3.jp>
2023-08-25 17:01:52 +0900

    set type for email in post create route

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -2,17 +2,38 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
 import prisma from '../../../lib/prisma';
 
+type CreatePostData = {
+  title: string;
+  description: string;
+  body: string;
+  author?: {
+    connect: {
+      email: string;
+    };
+  } | null;
+};
+
+
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
   const { title, body, description } = req.body;
 
   const session = await getSession({ req });
+
+  const postData: CreatePostData = {
+    title: title,
+    description: description,
+    body: body,
+  };
+
+  if (session?.user) {
+    postData.author = { connect: { email: session.user.email as string } };
+  } else {
+    postData.author = null;
+  }
+
   const result = await prisma.post.create({
-    data: {
-      title: title,
-      description: description,
-      body: body,
-      author: { connect: { email: session?.user?.email } },
-    },
+    data: postData,
   });
+
   res.json(result);
 }
